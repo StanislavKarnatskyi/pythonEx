8739e94066375ed6c12ff183dc3f19e4d5ffa1fa
ltpck <46033836+StanislavKarnatskyi@users.noreply.github.com>
2024-04-23 17:59:34 +0300

    rear

--- rearrangement.py
+++ rearrangement.py
@@ -1,5 +1,15 @@
 user_word = input("Enter the word: ")
-user_word_aranged = []
+user_word_arranged = []
+user_word_sym = []
+user_word_sym2 = []
+user = ""
+for i in user_word:
+    user_word_sym += i
+user = "".join(user_word_sym)
+print(user_word_sym, user)
+    #user_word_aranged += [user_word]
+#print(user_word_aranged)
+
 for i in range(len(user_word)):
-    user_word_aranged += [user_word]
-print(user_word_aranged)
+    user_word_sym2 += user_word_sym[-i+1]
+print(user_word_sym2)
