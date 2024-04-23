697f34bf9deee380a2ea60a0696f5b0de54cd929
StanislavKarnatskyi <46033836+StanislavKarnatskyi@users.noreply.github.com>
2024-04-23 22:06:10 +0300

    permutations
    Task: Write a program that generates all possible permutations of a given sequence of characters.
    
    Description: The user enters a sequence of characters (eg "abc") and the program must generate all possible permutations of these characters (in the case of "abc": "abc", "acb", "bac", "bca", "cab", "cba").

--- rearrangement.py
+++ rearrangement.py
@@ -1,15 +1,19 @@
-user_word = input("Enter the word: ")
-user_word_arranged = []
-user_word_sym = []
-user_word_sym2 = []
-user = ""
-for i in user_word:
-    user_word_sym += i
-user = "".join(user_word_sym)
-print(user_word_sym, user)
-    #user_word_aranged += [user_word]
-#print(user_word_aranged)
+word = input()
+list_mover = []
+temp = ""
+list_container = []
+list_sliced = []
 
-for i in range(len(user_word)):
-    user_word_sym2 += user_word_sym[-i+1]
-print(user_word_sym2)
+to_list = list(word)
+print(to_list)
+
+for i in range(len(word)):
+    to_list = [to_list[-1]] + to_list[:-1]
+    list_sliced = to_list[1:]
+    for j in range(len(word) - 2):
+        list_sliced = [list_sliced[-1]] + list_sliced[:-1]
+        temp = to_list[0] + "".join(list_sliced)
+        list_container += [temp]
+    list_container.append("".join(to_list))
+
+print(list_container)
